b2bb2f207b028a289c8d6362f9a93e6228ea9c9e
epamemo <epamemo@gmail.com>
2021-08-01 17:53:15 +0700

    next installasi template ejs

--- express-basic/apps.js
+++ express-basic/apps.js
@@ -5,6 +5,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/:name", (req, res) => res.send(`Nama saya : ${req.params.name}`));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
